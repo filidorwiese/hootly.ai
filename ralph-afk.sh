0eb34abeba16fe3fb295b95b58610f2badfd7d70
Filidor Wiese <fili@fili.nl>
2026-01-29 13:34:04 +0100

    Updates ralph setup

--- ralph-afk.sh
+++ ralph-afk.sh
@@ -11,11 +11,12 @@ for ((i=1; i<=$1; i++)); do
   result=$(~/.claude/local/claude --permission-mode acceptEdits "@plans/prd.json @progress.txt \
   1. Find the highest-priority feature to work on and work only on that feature.\
   This should the one YOU decide has the highest priority - not necessarily the first one listed in the PRD. \
-  2. Check that there are no type errors via npm typecheck and make sure tests pass via npm test. \
-  3. Update the PRD with the work that was done. \
-  4. Append your progress to the progress.txt file. \
+  2. Write unit-tests and/or e2e-tests using vitest for the feature. \
+  3. Check that there are no type errors via npm typecheck and make sure tests pass via npm test. \
+  4. Update the PRD with the work that was done. \
+  5. Append your progress to the progress.txt file. \
   Use this to leave a note for the next person working in the codebase. \
-  5. Make a git commit of that feature. \
+  6. Make a git commit of that feature. \
   ONLY WORK ON A SINGLE FEATURE. \
   If, while implementing the feature, you notice the PRD is complete, output <promise>COMPLETE</promise>.")
 
